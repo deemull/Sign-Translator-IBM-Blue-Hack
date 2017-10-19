9d5a6c6657602de74fa065688fc0409da3dc3afc
wwalker2 <wwalker2@xula.edu>
2017-10-19 15:19:30 -0400

    First Commit
    The first commit for this app.

---
+++ IBM_BlueHackAtlanta_SignTranslate/app/src/androidTest/java/bluehackatlanta/ibm_bluehackatlanta_signtranslate/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package bluehackatlanta.ibm_bluehackatlanta_signtranslate;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("bluehackatlanta.ibm_bluehackatlanta_signtranslate", appContext.getPackageName());
+    }
+}
---
+++ IBM_BlueHackAtlanta_SignTranslate/app/src/main/java/bluehackatlanta/ibm_bluehackatlanta_signtranslate/MainActivity.java
@@ -0,0 +1,16 @@
+package bluehackatlanta.ibm_bluehackatlanta_signtranslate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
